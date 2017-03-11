3ec990d71f0b5c30d5d6792b91f030eebf7750dd
akriti <akritikts@gmail.com>
2017-03-11 17:37:26 +0530

    config added

---
+++ app/src/main/java/in/silive/hackerearthgyanmatrix/utils/Config.java
@@ -0,0 +1,9 @@
+package in.silive.hackerearthgyanmatrix.utils;
+
+/**
+ * Created by akriti on 11/3/17.
+ */
+
+public class Config {
+    public static final String API_KEY = "http://hackerearth.0x10.info/api/gyanmatrix?type=json&query=list_player";
+}
