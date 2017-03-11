8dbbd55e4d75b0323a3234322006f0fdec5d3c7b
akriti <akritikts@gmail.com>
2017-03-11 16:28:38 +0530

    splash transition to mainActivity added

--- app/src/main/java/in/silive/hackerearthgyanmatrix/activities/SplashScreen.java
+++ app/src/main/java/in/silive/hackerearthgyanmatrix/activities/SplashScreen.java
@@ -1,15 +1,65 @@
 package in.silive.hackerearthgyanmatrix.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import in.silive.hackerearthgyanmatrix.R;
 
 public class SplashScreen extends AppCompatActivity {
+    Context context;
+    RelativeLayout splash;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        context = getApplicationContext();
+        checkConnection();
+    }
+
+    public void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        if (info == null) {
+            //   Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+            //no_net_connection.setVisibility(View.VISIBLE);
+            Snackbar snackbar = Snackbar
+                    .make(splash, "No internet connection!", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("RETRY", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            checkConnection();
+                        }
+                    });
+// Changing message text color
+            snackbar.setActionTextColor(Color.RED);
+// Changing action button text color
+            View sbView = snackbar.getView();
+            TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.YELLOW);
+            snackbar.show();
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //shimmer.cancel();
+
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 3000);
+        }
     }
 }
